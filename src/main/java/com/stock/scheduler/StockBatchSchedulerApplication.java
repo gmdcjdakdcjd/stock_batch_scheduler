ebbdeaed06af1ca4862ddae5f5bbbda84f5495e0
gmdcjdakdcjd <ysh010223@naver.com>
2025-10-01 22:01:10 +0900

    Init Spring Boot scheduler project

---
+++ src/main/java/com/stock/scheduler/StockBatchSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.stock.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockBatchSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockBatchSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stock/scheduler/StockBatchSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockBatchSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
